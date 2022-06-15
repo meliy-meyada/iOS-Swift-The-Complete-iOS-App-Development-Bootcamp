bbea09ccd54d1aa82c52fbcded8587aea3157e79
Meyada <79361511+meliy-meyada@users.noreply.github.com>
2022-06-15 19:52:47 +0700

    I Am Rich

---
+++ I am Rich/I am Rich/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by เมญาดา สายสัญ on 15/6/2565 BE.
+//
+
+import UIKit
+
+
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+
+
+}
+
