0169e1c39422e0b06619ad0daf7953073e5b37a8
Meyada <79361511+meliy-meyada@users.noreply.github.com>
2022-06-16 18:21:48 +0700

    Exercise : I Am Poor

---
+++ I Am Poor/I Am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by เมญาดา สายสัญ on 16/6/2565 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
